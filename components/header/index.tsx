41b489bacad01b17d86b4784ddf1ae5e3865c357
saliharya <saliharyazinc@gmail.com>
2024-07-09 17:44:20 +0700

    add arrow button

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -5,12 +5,17 @@ const Header = () => {
         <header className="bg-white h-48 absolute top-0 left-0 w-full">
             <div className="container mx-auto flex items-center justify-between px-6 h-full">
                 <div className="flex items-center">
-                    <img src="/logo.png" alt="Logo" className="mr-4 h-36" />
-                    <input
-                        type="text"
-                        placeholder="Cari negara, kota, atau guide"
-                        className="border rounded-full px-4 py-2 focus:outline-none focus:ring focus:border-blue-300 text-lg shadow-md custom-shadow w-96 h-16"
-                    />
+                    <img src="/logo.png" alt="Logo" className="mr-4 h-24" />
+                    <div className="relative">
+                        <input
+                            type="text"
+                            placeholder="Cari negara, kota, atau guide"
+                            className="border rounded-full px-4 py-2 focus:outline-none focus:border-blue-300 text-lg shadow-md custom-shadow w-96 h-12 pr-12"
+                        />
+                        <button className="absolute inset-y-0 right-0 flex items-center justify-center bg-transparent pr-1 py-1">
+                            <img src="/arrow-right.svg" alt="Search" className="w-full h-full" />
+                        </button>
+                    </div>
                 </div>
                 <nav className="flex space-x-6">
                     <Link href="/" className="text-red-500 text-2xl">
